818bbdafa54366922b5eb5b1db69c46f28e6ed44
Mathias <mbrix@student.sdu.dk>
2022-05-08 16:19:12 +0200

    Ass

--- sensors/main.py
+++ sensors/main.py
@@ -2,45 +2,44 @@ import VEML7700
 import SCD41
 import time
 
-on = False
-rate = 1
-counter = 0
-_scd41 = None
-_veml7700 = None
+class Man():
+  on = False
+  rate = 1
+  counter = 0
+  scd41 = None
+  veml7700= None
 
-co2 = None
-temperature = None
-humidity = None
-lux = None
-light = None
+  co2 = None
+  temperature = None
+  humidity = None
+  lux = None
+  light = None
 
-def printSamples():
-  print("light: " , light)
-  print("Lux: ", lux)
-  print("Temperature: ", temperature)
-  print("Co2: ", co2)
-  print("Humidity: " , humidity)
+  def __init__(self):
+        self.scd41=SCD41.init()
+        self.veml7700=VEML7700.init()
+        self.on = True
 
-def setup():
-  _scd41 = SCD41.init()
-  _veml7700 = VEML7700.init()
-  on = True
+  def printSamples():
+    print("light: " , Man.light)
+    print("Lux: ", Man.lux)
+    print("Temperature: ", Man.temperature)
+    print("Co2: ", Man.co2)
+    print("Humidity: " , Man.humidity)
 
-setup()
+  while(on):
+    #sample CO2
+    co2 = SCD41.sampleCo2(scd41)
+    #sample Temperature
+    temperature = SCD41.sampleTemperature(scd41)
+    #sample Humanity
+    humidity = SCD41.sampleHumidity(scd41)
+    #sample Light and Lux
+    light, lux = VEML7700.sampleLight(veml7700)
+    counter += 1
 
-while(on):
-  #sample CO2
-  co2 = SCD41.sampleCo2(_scd41)
-  #sample Temperature
-  temperature = SCD41.sampleTemperature(_scd41)
-  #sample Humanity
-  humidity = SCD41.sampleHumidity(_scd41)
-  #sample Light and Lux
-  light, lux = VEML7700.sampleLight(_veml7700)
-  counter += 1
-
-  time.sleep(rate)
-  printSamples()
-  
-  if(counter == 1000):
-   on = False
+    time.sleep(rate)
+    printSamples()
+    
+    if(counter == 1000):
+      on = False
