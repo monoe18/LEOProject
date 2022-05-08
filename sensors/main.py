b819766f007e62d760e16d055735d46eed18af36
Mathias <mbrix@student.sdu.dk>
2022-05-08 16:09:37 +0200

    Hey

--- sensors/main.py
+++ sensors/main.py
@@ -4,7 +4,7 @@ import time
 
 on = False
 rate = 1
-
+counter = 0
 _scd41 = None
 _veml7700 = None
 
@@ -14,23 +14,33 @@ humidity = None
 lux = None
 light = None
 
+def printSamples():
+  print("light: " , light)
+  print("Lux: ", lux)
+  print("Temperature: ", temperature)
+  print("Co2: ", co2)
+  print("Humidity: " , humidity)
+
 def setup():
   _scd41 = SCD41.init()
   _veml7700 = VEML7700.init()
   on = True
 
-def loop():
-  while(on):
+setup()
+
+while(on):
   #sample CO2
-    co2 = SCD41.sampleCo2(_scd41)
+  co2 = SCD41.sampleCo2(_scd41)
   #sample Temperature
-    temperature = SCD41.sampleTemperature(_scd41)
+  temperature = SCD41.sampleTemperature(_scd41)
   #sample Humanity
-    humidity = SCD41.sampleHumidity(_scd41)
-    
+  humidity = SCD41.sampleHumidity(_scd41)
   #sample Light and Lux
-    light, lux = VEML7700.sampleLight(_veml7700)
+  light, lux = VEML7700.sampleLight(_veml7700)
+  counter += 1
 
-    time.sleep(rate)
-
-
+  time.sleep(rate)
+  printSamples()
+  
+  if(counter == 1000):
+   on = False
