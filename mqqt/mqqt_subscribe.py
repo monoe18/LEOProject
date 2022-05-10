52407140928013cca92fe97bed29ecce8f247489
Daniel Szenczi <dasze18@student.sdu.dk>
2022-05-10 14:33:52 +0200

    changes

--- mqqt/mqqt_subscribe.py
+++ mqqt/mqqt_subscribe.py
@@ -1,19 +1,19 @@
-import paho.mqtt.publish as publish
+import paho.mqtt.client as mqtt
 from flask import Flask
 
 app = Flask(__name__)
-
+print("start life")
 def messageFunction (client, userdata, message):
     topic = str(message.topic)
- 	message = str(message.payload.decode(“utf-8”))
- 	print(topic + message)
+    message = str(message.payload.decode("utf-8"))
+    print(topic +":" + message)
      
-ourClient = mqtt.Client(“LEOFlaskF22”)
-ourClient.connect(“test.mosquitto.org”, 1883)
-ourClient.subscribe(“LEODataF22”)
+ourClient = mqtt.Client("LEOFlaskF22")
+ourClient.connect("test.mosquitto.org", 1883)
+ourClient.subscribe("LEODataF22")
 ourClient.on_message = messageFunction # Attach the messageFunction to subscription
 ourClient.loop_start() # Start the MQTT client
-
+print("life life ")
 
 @app.route("/")
 def hello_world():
