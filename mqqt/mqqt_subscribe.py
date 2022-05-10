0d123ce4e7f11f4a45087a62ed5735dd87683843
Morten <monoe18@student.sdu.dk>
2022-05-10 15:00:43 +0200

    flask status

--- mqqt/mqqt_subscribe.py
+++ mqqt/mqqt_subscribe.py
@@ -1,20 +1,35 @@
 import paho.mqtt.client as mqtt
 from flask import Flask
+MQTT_TOPIC = [("LEODataF22/Temperature"),("LEODataF22/Humidity"),("LEODataF22/Light"),("LEODataF22/Co2"),("LEODataF22/Lux")]
+temperature = 0
+humidity = 0
+light = 0
+Co2 = 0
+lux = 0
 
 app = Flask(__name__)
 print("start life")
 def messageFunction (client, userdata, message):
-    topic = str(message.topic)
-    message = str(message.payload.decode("utf-8"))
-    print(topic +":" + message)
+    if(str(message.topic) == "LEODataF22/Temperature"):
+        temperature = message.payload.decode("utf-8")
+    elif (str(message.topic) == "LEODataF22/Humidity"):
+        humidity = message.payload.decode("utf-8")
+    elif (str(message.topic) == "LEODataF22/Light"):
+        light = message.payload.decode("utf-8")
+    elif (str(message.topic) == "LEODataF22/Co2"):
+        Co2 = message.payload.decode("utf-8")
+    elif (str(message.topic) == "LEODataF22/Lux"):
+        lux = message.payload.decode("utf-8")
+    
      
 ourClient = mqtt.Client("LEOFlaskF22")
 ourClient.connect("test.mosquitto.org", 1883)
-ourClient.subscribe("LEODataF22")
+ourClient.subscribe(MQTT_TOPIC)
+
 ourClient.on_message = messageFunction # Attach the messageFunction to subscription
 ourClient.loop_start() # Start the MQTT client
 print("life life ")
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<p>"+ "Temp: " + temperature + "</p> \n<p>"+ "Humi: " + humidity + "</p> \n<p>"+ "Light: "  + light + "</p> \n<p>"+ "Lux: " + lux + "</p> \n<p>"+ "Co2: " + Co2 + "</p> \n"
