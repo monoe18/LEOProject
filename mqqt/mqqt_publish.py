4d424c5e0c3c7acfac46dd7639aa4d88ab02b16d
Daniel Szenczi <dasze18@student.sdu.dk>
2022-05-10 14:38:17 +0200

    changes23

--- mqqt/mqqt_publish.py
+++ mqqt/mqqt_publish.py
@@ -1,4 +1,4 @@
-import paho.mqtt.publish as publish
+import paho.mqtt.client as mqtt
 ourClient = mqtt.Client("LEOPI")
 ourClient.connect("test","mosquitto.org", 1883)
 ourClient.publish("Data", "ThisIsATest") 
