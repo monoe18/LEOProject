c40995deff8142fed25e3038940a249e7b1f1ec2
Mathias <mbrix@student.sdu.dk>
2022-05-16 16:10:55 +0200

    All sensors

---
+++ sensors/mqqt_publish_test.py
@@ -0,0 +1,24 @@
+from random import randint
+from time import sleep
+import time
+import paho.mqtt.publish as publish
+
+while True:
+    time.sleep(1)
+    value1 = randint(0, 50)
+    publish.single("group07/Humidity", value1 , hostname="3.73.44.177", port=1883, auth={'username':"group07", 'password':"raspberry"})
+    print("Humidity: ", value1)
+    value2 = randint(0, 50)
+    publish.single("group07/Temperature", value2 , hostname="3.73.44.177", port=1883, auth={'username':"group07", 'password':"raspberry"})
+    print("Temperature: ", value2)
+    time.sleep(1)
+    value3 = randint(0, 50)
+    publish.single("group07/C02", value3 , hostname="3.73.44.177", port=1883, auth={'username':"group07", 'password':"raspberry"})
+    print("Humidity: ", value3)
+    value4 = randint(0, 50)
+    publish.single("group07/Lux", value4 , hostname="3.73.44.177", port=1883, auth={'username':"group07", 'password':"raspberry"})
+    print("Temperature: ", value4)
+    value5 = randint(0, 50)
+    publish.single("group07/Light", value5 , hostname="3.73.44.177", port=1883, auth={'username':"group07", 'password':"raspberry"})
+    print("Temperature: ", value5)
+ 
