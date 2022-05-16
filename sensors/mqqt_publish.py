b9d64fdc335c2efcdf114b79e06306444fd975ee
Mathias <mbrix@student.sdu.dk>
2022-05-16 15:41:49 +0200

    Test script

--- sensors/mqqt_publish.py
+++ sensors/mqqt_publish.py
@@ -1,5 +1,11 @@
+from random import randint
+from time import sleep
+import time
 import paho.mqtt.publish as publish
 
-publish.single("PlantHealth/temperature", "Hello", hostname="localhost")
-print("Done")
+while True:
+    time.sleep(1)
+    value = randint(0, 10)
+    publish.single("group07/test", value , hostname="3.73.44.177", port=1883, auth={'username':"group07", 'password':"raspberry"})
+    print(value)
  
