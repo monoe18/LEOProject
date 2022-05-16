5cf821287beae631057869e8761b78a063d6057e
Mathias <mbrix@student.sdu.dk>
2022-05-16 16:38:06 +0200

    D

--- sensors/mqqt_publish_test.py
+++ sensors/mqqt_publish_test.py
@@ -12,7 +12,7 @@ while True:
     publish.single("group07/Temperature", value2 , hostname="3.73.44.177", port=1883, auth={'username':"group07", 'password':"raspberry"})
     print("Temperature: ", value2)
     value3 = randint(0, 50)
-    publish.single("group07/C02", value3 , hostname="3.73.44.177", port=1883, auth={'username':"group07", 'password':"raspberry"})
+    publish.single("group07/CO2", value3 , hostname="3.73.44.177", port=1883, auth={'username':"group07", 'password':"raspberry"})
     print("C02: ", value3)
     value4 = randint(0, 50)
     publish.single("group07/Lux", value4 , hostname="3.73.44.177", port=1883, auth={'username':"group07", 'password':"raspberry"})
