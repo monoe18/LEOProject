773194c5a83c8002ba5fae8a2a3d76a6a1b114d6
Morten <monoe18@student.sdu.dk>
2022-05-17 11:24:42 +0200

    auths

--- sensors/mqtt_publish.py
+++ sensors/mqtt_publish.py
@@ -3,8 +3,9 @@ import paho.mqtt.client as mqtt
 def connect():
     c = mqtt.Client("LEOPI")
     c.connect("3.73.44.177", 1883)
+    c.username_pw_set("group07", "raspberry")
     return c
 
 def publishData(c, topicName, data):
     print("sending data to: " + "group07/"+topicName)
-    c.publish("group07/"+topicName, data, auth={'username':"group07", 'password':"raspberry"}) 
+    c.publish("group07/"+topicName, data) 
